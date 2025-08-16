fa3bd56f97917c7578654084cee170c63b98741b
Abdallah Safi <abdullahsf2001@gmail.com>
2025-08-16 14:41:55 -0500

    perf: optimize analytics dashboard for 10k+ events using data decimation

--- dashboard/src/hooks/index.ts
+++ dashboard/src/hooks/index.ts
@@ -2,3 +2,4 @@ export { usePipelineStatus, usePipelineRuns } from './usePipelineStatus';
 export { useDataQualityMetrics, useRecordCounts, usePipelineMetrics } from './useMetrics';
 export { useSearchFunnel, useTopQueries, useUserSegments, useSearch } from './useSearchAnalytics';
 export { useDebounce, useDebouncedCallback, usePolling, useLocalStorage, useMediaQuery, useClickOutside } from './useCustomHooks';
+export * from './usePerformanceMetrics';
